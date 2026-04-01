ac2ba6d8a84b60a0d5a99967a2e55acf5ddb07d5
Tommythewho12 <tommythewho12@googlemail.com>
2026-04-01 21:27:32 +0200

    refactor nodemailer

--- express-api/src/services/email-service.ts
+++ express-api/src/services/email-service.ts
@@ -1,7 +1,6 @@
 import nodemailer from "nodemailer";
-import type SMTPTransport from "nodemailer/lib/smtp-transport/index.js";
 
-const config: SMTPTransport.Options = {
+const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST || "mail.rwth-aachen.de",
     port: Number(process.env.MAIL_PORT) || 587,
     secure: false, // true for port 465, false for others
@@ -9,9 +8,7 @@ const config: SMTPTransport.Options = {
         user: process.env.MAIL_USER || "tk812812@rwth-aachen.de",
         pass: process.env.MAIL_PASS || "rwth_Naugatuck09"
     },
-}
-
-const transporter = nodemailer.createTransport(config);
+});
 
 const emailService = {
     sendMailPasswordReset: (address: string, pass: string) => {
